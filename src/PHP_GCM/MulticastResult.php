b844a55257951e116ca91e820bf4433892cf0eca
lkorth <luke@lukekorth.com>
2013-04-09 14:19:36 -0500

    Add MulticastResult class

---
+++ src/PHP_GCM/MulticastResult.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace PHP_GCM;
+
+/**
+ * Result of a GCM multicast message request.
+ */
+class MulticastResult {
+
+    private $success;
+    private $failure;
+    private $canonicalIds;
+    private $multicastId;
+    private $results;
+    private $retryMulticastIds;
+
+    /**
+     * @param int $success
+     * @param int $failure
+     * @param int $canonicalIds
+     * @param string $multicastId
+     * @param array $results
+     * @param array $retryMulticastIds
+     */
+    public function __construct($success, $failure, $canonicalIds, $multicastId, array $results,
+                                array $retryMulticastIds = array()) {
+        $this->success = $success;
+        $this->failure = $failure;
+        $this->canonicalIds = $canonicalIds;
+        $this->multicastId = $multicastId;
+        $this->results = $results;
+        $this->retryMulticastIds = $retryMulticastIds;
+    }
+
+    /**
+     * Gets the multicast id.
+     *
+     * @return string
+     */
+    public function getMulticastId() {
+        return $this->multicastId;
+    }
+
+    /**
+     * Gets the number of successful messages.
+     *
+     * @return int
+     */
+    public function getSuccess() {
+        return $this->success;
+    }
+
+    /**
+     * Gets the total number of messages sent, regardless of the status.
+     *
+     * @return int
+     */
+    public function getTotal() {
+        return $this->success + $this->failure;
+    }
+
+    /**
+     * Gets the number of failed messages.
+     *
+     * @return int
+     */
+    public function getFailure() {
+        return $this->failure;
+    }
+
+    /**
+     * Gets the number of successful messages that also returned a canonical
+     * registration id.
+     *
+     * @return int
+     */
+    public function getCanonicalIds() {
+        return $this->canonicalIds;
+    }
+
+    /**
+     * Gets the results of each individual message
+     *
+     * @return array
+     */
+    public function getResults() {
+        return $this->results;
+    }
+
+    /**
+     * Gets additional ids if more than one multicast message was sent.
+     *
+     * @return array
+     */
+    public function getRetryMulticastIds() {
+        return $this->retryMulticastIds;
+    }
+}
