900bc7d0389c0240a31776cb1aa1c7d97538ce3b
Ian Littman <ian@cloudyhills.com>
2015-05-26 12:38:32 -0500

    Fix docblocks

--- src/PHP_GCM/MulticastResult.php
+++ src/PHP_GCM/MulticastResult.php
@@ -34,7 +34,6 @@ class MulticastResult {
      * @param int $failure
      * @param int $canonicalIds
      * @param string $multicastId
-     * @param array $results
      * @param array $retryMulticastIds
      */
     public function __construct($success, $failure, $canonicalIds, $multicastId, array $retryMulticastIds = array()) {
